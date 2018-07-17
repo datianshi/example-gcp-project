edf155c844e53ff4d7d6d8db040414ca7fe6468a
Shaozhen Ding <dsz0111@gmail.com>
2018-07-17 15:53:00 -0600

    add projects

--- pcf-host-project/project.tf
+++ pcf-host-project/project.tf
@@ -1,5 +1,5 @@
-# resource "google_project" "pcf_project" {
-#   name = "${var.env_name}-pcf-project"
-#   project_id = "${var.project_id}"
-#   org_id     = "${var.org_id}"
-# }
+resource "google_project" "pcf_project" {
+  name = "${var.env_name}-pcf-project"
+  project_id = "${var.project_id}"
+  org_id     = "${var.org_id}"
+}
